f44a202881cbf2a1b47ef51b80ebf93b476b0e70
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2024-03-25 21:26:38 +0700

    feat: markdown content reader

---
+++ src/service/readMarkdown.js
@@ -0,0 +1,37 @@
+class MarkdownReader {
+  constructor() {
+    this.reader = new FileReader();
+  }
+
+  async readFile(file) {
+    return new Promise((resolve, reject) => {
+      this.reader.onload = function (event) {
+        const fileData = event.target.result;
+        resolve({data: fileData, name: file.name});
+      };
+      this.reader.onerror = function (event) {
+        reject(event);
+      };
+      this.reader.readAsText(file);
+    });
+  }
+
+  async readFiles(fileList) {
+    let promiseList = [];
+    for (let file of fileList) { 
+      promiseList.push(this.readFile(file));
+    }
+    return Promise.all(promiseList);
+  }  
+
+  parseToHTML(mdFileList) {    
+    let result = [];
+    for(let mdFile of mdFileList) {
+      let html = marked.parse(mdFile.data);  
+      let newName = mdFile.name.replace(/\.md$/, '.html');
+      result.push({name: newName, html});
+    }
+    return result;
+  }
+
+}
