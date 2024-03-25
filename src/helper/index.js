36247b27150d58ecdbc13047e34e7ffe1b36df68
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2024-03-25 14:30:58 +0700

    feat: no content file name overlap

--- src/helper/index.js
+++ src/helper/index.js
@@ -3,9 +3,30 @@ function validateSize(inp, limit) {
   if (inp.files.length !== 0) {
     if(inp.files[0].size > limit){
         inp.value = ""; 
-        alert("File is too big!");
         return false;
     };  
   } 
-  return true;
+  return true; 
+} 
+
+function isFileNameOverlap(fileList){   
+  let fileNames = fileList.map(file => file.name);  
+  let isOverlap = false;    
+  let nameMap = {} 
+
+  for(file of fileNames){
+    if(nameMap.hasOwnProperty(file)){
+      isOverlap = true;
+      break;
+    }else{
+      nameMap[file] = true;
+    }
+  }
+  return isOverlap;
+} 
+
+
+module.exports = { 
+  validateSize, 
+  isFileNameOverlap
 }
